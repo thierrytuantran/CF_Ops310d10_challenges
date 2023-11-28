2b05fe8306d266b1d71e2e2c4b844acafe0cb41c
Thierry Tran <anhtuan1996tran@gmail.com>
2023-11-28 14:41:21 -0800

    Add Date_file.sh script

---
+++ Date_file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set source file and destination directory
+source_file="/var/log/syslog"
+destination_dir="./"
+
+# Create a timestamp for the filename
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+# Build the destination filename with the timestamp
+destination_file="${destination_dir}syslog_${timestamp}.log"
+
+# Copy the source file to the destination with the timestamped filename
+cp "$source_file" "$destination_file"
+
+# Display a message indicating the copy operation
+echo "Log file copied to: $destination_file"
