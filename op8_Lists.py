d54305c2faacd0dc00e18a1a111ce4317c733b00
Thierry Tran <131303287+tt11223@users.noreply.github.com>
2023-12-06 21:49:24 -0800

    Create op8_Lists.py

---
+++ op8_Lists.py
@@ -0,0 +1,14 @@
+# Assign a list of ten string elements to a variable
+fruits = ["Tomato", "Mango", "Apple", "Peach", "Watermelon", "Pineapple", "Grape", "Lemon", "Avocado", "Cherry"]
+
+# Print the fourth element of the list
+print("Fourth element:", fruits[3])
+
+# Print the sixth through tenth element of the list
+print("Sixth through tenth elements:", fruits[5:])
+
+# Change the value of the seventh element to "onion"
+fruits[6] = "Onion"
+
+# Print the updated list
+print("Updated list:", fruits)
