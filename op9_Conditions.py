6c23560746a0ba7716b89704ef6f143a2de75fe0
Thierry Tran <131303287+tt11223@users.noreply.github.com>
2023-12-07 19:22:02 -0800

    Create op9_Conditions.py

---
+++ op9_Conditions.py
@@ -0,0 +1,51 @@
+# Initiate the value of a and b
+# Given variables
+a = 5
+b = 10
+c = 8
+
+# Equals: a == b
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+
+# Not Equals: a != b
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+
+# Less than: a < b
+if a < b:
+    print("a is less than b")
+else:
+    print("a is not less than b")
+
+# Less than or equal to: a <= b
+if a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is greater than b")
+
+# Greater than: a > b
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+
+# Greater than or equal to: a >= b
+if a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("a is less than b")
+
+# Using if, elif, and else
+
+if a > c:
+    print("a is greater than c")
+elif a == c:
+    print("a is equal to c")
+else:
+    print("a is less than c")
+
